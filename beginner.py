f0a712233251ae687291ddfb64b7b05c2cbe548b
Elmer Catapang <ecatapang@belusallc.com>
2024-03-08 18:01:11 -0500

    code changes

--- beginner.py
+++ beginner.py
@@ -1,5 +1,4 @@
-Thomas_Age = 10
-Age_at_Kindergarten = 5
-
-if Thomas_Age < Age_at_Kindergarten:
-    print("Thomas should be in pre-school")
+firstname = "Elmer"
+lastname = "Catapang"
+fullname = firstname + " " + lastname
+print("hello", fullname)
